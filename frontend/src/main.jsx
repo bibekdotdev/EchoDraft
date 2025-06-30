ec395665c4a25c8c6bc9a9b06a506b636f94d1c2
Bibek Jana <168276117+bibekdotdev@users.noreply.github.com>
2025-06-30 08:57:40 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,9 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY =
-  "pk_test_a25vd2luZy10aWNrLTg5LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
